af4630e59867361d1c3dc5227db7049047c86fd7
RaulJimenezOrtega <hhkaos@gmail.com>
2014-06-19 03:53:57 +0200

    Finishing first version

--- extension/content_script.js
+++ extension/content_script.js
@@ -12,7 +12,7 @@ if(!document.getElementById("style")){
 	style.href = "";
 	document.documentElement.firstChild.appendChild(style);
 	console.log("no existe")
-
+/*
 	$("body").prepend(""+
 		"<form id='styler'>"+
 		"	<div id='close'>Close</div>"+
@@ -27,12 +27,28 @@ if(!document.getElementById("style")){
 	$("body").append("<script src='//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>")
 	var loadBehaviour = function(){
 		$("body").append("<script src='http://hhkaos.github.io/arcgis-organization-styler/resources/scripts.js'></script>");
+	};*/
+
+	$("body").prepend(""+
+		"<form id='styler'>"+
+		"	<div id='close'>Hide styler</div>"+
+		//"	<p>Custom style css:</p>"+
+		"	<button id='getCode'>Get the code</button>"+
+		"	<p>Snippets: </p><div id='snippets-selector'><select id='snippets'><option>Loading...</option></select><button id='add-snippet'>Add</button></div>"+
+		"	<ul id='selector'><li id='css' class='active'>CSS</li><li id='html'>HTML</li></ul>"+
+		"	<textarea id='css-style'></textarea><textarea style='display:none' id='html-content'></textarea><br>"+
+		"	<button id='submit'>Apply styles</button>"+
+		"</form><div id='basic-modal-content'><p>Instructions: 1) Go to My Organization > Edit Settings > Home Page tab, 2) In the Banner section, choose the \"HTML\" radio button and click the \"View HTML Source\" button (the last button on the Banner box's toolbar), 3) Paste the HTML code below into the text field after/below the existing code </p><textarea></textarea><div><p><button>Close</button></p></div>");
+	$("body").append("<link id='style' type='text/css' rel='stylesheet' href='http://localhost:9090/resources/styles.css'>")
+	$("body").append("<style id='custom-style' type='text/css'></style>")
+	$("body").append("<script src='//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>")
+
+	var loadBehaviour = function(){
+		$("body").append("<script src='http://localhost:9090/resources/scripts.js'></script>");
 	};
 
 	setTimeout(loadBehaviour, 2000);
 	
 	
 
-}else
-	console.log("ya existe")
-
+}
--- resources/scripts.js
+++ resources/scripts.js
@@ -1,77 +1,93 @@
 $(document).ready(function(){
-		var proxy = "http://www.corsproxy.com/";
-		var snippets = [];
-		//Apply the current styples
-		$("#styler").submit(function(e){
-			e.preventDefault();
-			$("#custom-style").html($("#css-style").val());
-			
-			if($("#custom-html").length == 0){
-				$("#banner-html").prepend("<div id='custom-html'></div>");
-			}
-			$("#custom-html").html($("#html-content").val());
-			
-		});
-
-		//Loads all the available snippets
-		$.ajax({
-			url: proxy + "hhkaos.github.io/arcgis-organization-styler/snippets/snippets.json",
-			dataType: "json",
-			success: function(data){
-				snippets = data.snippets;
-				var elems = data.snippets.length;
-				for(i = 0; i < elems; i++){
-					$("#snippets").append("<option value='"+ i +"'>" + data.snippets[i].name + "</option>");
-				}
-				console.log("data=",data);
-			}
-		});
-
-		//Add the snippet to the textarea
-		$("#add-snippet").click(function(){
-			var obj = snippets[$("#snippets").val()];
-
-			if(obj.hasOwnProperty("css")){
-				//Get the CSS
-				$.ajax({
-					url: proxy + obj.css,
-					//dataType: "json",
-					success: function(data){
-						
-						$("#css-style").val($("#css-style").val()+"\n"+data);
-					}
-				});
-			}
-			if(obj.hasOwnProperty("html")){
-				//Get the HTML
-				$.ajax({
-					url: proxy + obj.html,
-					//dataType: "json",
-					success: function(data){
-						
-						$("#html-content").val($("#html-content").val()+"\n"+data);
-					}
-				});
-			}
-
-
-		});
-
-		//Switch the textarea active
-		$("#selector li").click(function(){
-			var newActive = $(this).attr("id");
-			$("#selector .active").removeClass("active");
-			$(this).addClass("active");
-			if(newActive == "css"){
-				$("#html-content").hide();
-				$("#css-style").show();
-
-			}else if(newActive == "html"){
-				$("#html-content").show();
-				$("#css-style").hide();
-			}
-		});
+	var proxy = "http://www.corsproxy.com/";
+	var snippets = [];
+	//Apply the current styples
+	$("#styler").submit(function(e){
+		e.preventDefault();
+		$("#custom-style").html($("#css-style").val());
+		
+		if($("#custom-html").length == 0){
+			$("#banner-html").prepend("<div id='custom-html'></div>");
+		}
+		$("#custom-html").html($("#html-content").val());
+		
 	});
+
+	//Loads all the available snippets
+	$.ajax({
+		url: proxy + "hhkaos.github.io/arcgis-organization-styler/snippets/snippets.json",
+		dataType: "json",
+		success: function(data){
+			snippets = data.snippets;
+			var elems = data.snippets.length;
+			$("#snippets").empty();
+			for(i = 0; i < elems; i++){
+				$("#snippets").append("<option value='"+ i +"'>" + data.snippets[i].name + "</option>");
+			}
+		}
+	});
+
+	//Add the snippet to the textarea
+	$("#add-snippet").click(function(){
+		var obj = snippets[$("#snippets").val()];
+
+		if(obj.hasOwnProperty("css")){
+			//Get the CSS
+			$.ajax({
+				url: proxy + obj.css,
+				//dataType: "json",
+				success: function(data){
+					
+					$("#css-style").val($("#css-style").val()+"\n"+data);
+					$("#submit").click();
+				}
+			});
+		}
+		if(obj.hasOwnProperty("html")){
+			//Get the HTML
+			$.ajax({
+				url: proxy + obj.html,
+				//dataType: "json",
+				success: function(data){
+					
+					$("#html-content").val($("#html-content").val()+"\n"+data);
+					$("#submit").click();
+				}
+			});
+		}
+
+
+	});
+
+	//Switch the textarea active
+	$("#selector li").click(function(){
+		var newActive = $(this).attr("id");
+		$("#selector .active").removeClass("active");
+		$(this).addClass("active");
+		if(newActive == "css"){
+			$("#html-content").hide();
+			$("#css-style").show();
+
+		}else if(newActive == "html"){
+			$("#html-content").show();
+			$("#css-style").hide();
+		}
+	});
+
+
+	//Displays the code
+	$("#getCode").click(function(){
+		var tmp = "<style type='text/css'>\n";
+		tmp += '$("#css-style").val()';
+		tmp += "\n</style>\n";
+		tmp += $("#html-content").val();
+		$("#basic-modal-content textarea").val(tmp);
+		$("#basic-modal-content").show()
+	});
+	$("#basic-modal-content button").click(function(){
+		$("#basic-modal-content").hide()
+	});
+});
 
 $(document).delegate('#styler textarea', 'keydown', function(e) {
   var keyCode = e.keyCode || e.which;
@@ -96,7 +112,9 @@ $(document).delegate('#styler textarea', 'keydown', function(e) {
 $("#close").click(function(){
 	if($("#styler").hasClass("closed")){
 		$("#styler").removeClass("closed");	
+		$("#close").text("Hide styler");
 	}else{
 		$("#styler").addClass("closed")
+		$("#close").text("Show");
 	}
 });
