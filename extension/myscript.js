d52c8ebc1fe316c101ad7d47c4c57ac15adf6d4d
RaulJimenezOrtega <hhkaos@gmail.com>
2014-06-19 11:15:32 +0200

    Cleaning the repo

--- extension/myscript.js
+++ extension/myscript.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
-	
 	chrome.tabs.executeScript(null, {file: "jquery-1.11.1.min.js"});
-	chrome.tabs.executeScript(null, {file: "jquery.tmpl.min.js"});
 	chrome.tabs.executeScript(null, {file: "content_script.js"});
 });
 
